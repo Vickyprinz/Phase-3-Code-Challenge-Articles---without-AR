a34e917770322a3864b26139b070756106602433
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-20 11:02:44 +0300

    created helper function

--- lib/article.rb
+++ lib/article.rb
@@ -7,4 +7,18 @@ class Magazine
       @category = category
       ##shovel each new instance into an array
       @@all << self
+
+
+    def self.all
+        @@all
+      end
+
+         ##helper function
+      def magazine_articles
+        Article.all.select{|article| article.magazine.name == self.name}
+      end
+
+      def contributors
+        magazine_articles.collect{|article| article.author}.uniq
+      end
     end
