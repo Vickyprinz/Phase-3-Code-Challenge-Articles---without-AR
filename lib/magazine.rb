a474d9c93e9662b8ea69235ed11a6ab74b5800ba
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-20 11:08:43 +0300

    added class magazine

---
+++ lib/magazine.rb
@@ -0,0 +1,10 @@
+class Magazine
+    attr_accessor :name, :category
+    @@all = []
+
+    def initialize(name, category)
+      @name = name
+      @category = category
+      ##shovel each new instance into an array
+      @@all << self
+    end
