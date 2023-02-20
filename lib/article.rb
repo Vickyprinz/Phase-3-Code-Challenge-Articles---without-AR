8881f35007e61204a4d1f2bf08d62888488d2a97
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-20 13:00:24 +0300

    Implement Article class to track articles across magazines

--- lib/article.rb
+++ lib/article.rb
@@ -2,5 +2,10 @@ class Article
 
 attr_reader :magazine, :title, :author
     @@all = []
-    
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+      end
 end
