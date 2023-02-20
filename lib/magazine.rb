ec8acbd2014280ce9245ef01a1f401ac11f418f3
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-20 12:53:57 +0300

    Add updated magazine class

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -1,40 +1,58 @@
 class Magazine
     attr_accessor :name, :category
     @@all = []
-
+  
     def initialize(name, category)
       @name = name
       @category = category
-      ##shovel each new instance into an array
       @@all << self
     end
+  
     def self.all
-        @@all
-      end
-
-         ##helper function
-      def magazine_articles
-        Article.all.select{|article| article.magazine.name == self.name}
-      end
-
-      def contributors
-        magazine_articles.collect{|article| article.author}.uniq
-      end
-      def contributors
-        magazine_articles.collect{|article| article.author}.uniq
-      end
-
-      def self.find_by_name(name)
-        find_magazines = self.all.select { |magazine| magazine.name == name}
-        find_magazines.first
-      end
-      def article_titles
-        magazine_articles.collect{|article| article.title}
-      end
-      def contributing_authors
-        #tally authors then map through the articles and get array of authors
-        magazine_authors = magazine_articles.collect{|article| article.author.name}
-        magazine_authors.tally.each {|key, value|  value > 2}
-        magazine_authors
-      end
+      @@all
     end
+  
+    def articles
+      Article.all.select { |article| article.magazine == self }
+    end
+  
+    def authors
+      articles.collect { |article| article.author }.uniq
+    end
+  
+    def article_titles
+      articles.collect { |article| article.title }
+    end
+  
+    def author_names
+      authors.collect { |author| author.name }
+    end
+  
+    def contributor_names
+      Article.all.filter { |article| article.magazine == self }.map { |article| article.author.name }.uniq
+    end
+  
+    def self.find_by_name(name)
+      self.all.find { |magazine| magazine.name == name }
+    end
+  
+    def self.find_by_category(category)
+      self.all.select { |magazine| magazine.category == category }
+    end
+  
+    def self.most_popular
+      self.all.max_by { |magazine| magazine.contributor_names.count }
+    end
+  
+    def contributor_count
+      contributor_names.count
+    end
+  
+    def contributing_authors
+      popular_authors = Article.all.filter { |article| article.magazine.name == @name }.map {
+        |article|
+        article.author.name
+      }.tally.each { |key, value| value > 2 }
+      popular_authors
+    end
+  end
