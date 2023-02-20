71ec9ed0196a41dd38482d0c0751c48a2c6498f5
git_Vickyprinz <kariukivictor315@gmail.com>
2023-02-20 12:42:33 +0300

    Add articles method to Author class to return all articles written by author

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,13 @@
 class Author
     attr_reader :name
 
+    def initialize(name)
+        @name = name
+      end
+    
+      def articles
+        Article.all.select{|article| article.author.name == self.name}
+      end
+    
 
 end
